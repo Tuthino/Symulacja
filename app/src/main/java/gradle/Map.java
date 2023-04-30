380a98628d53784bb674dd43ec5544a174f60c21
Reyio <reyio@vivaldi.net>
2023-04-30 17:09:04 +0200

    Create starting boxes

--- app/src/main/java/gradle/Map.java
+++ app/src/main/java/gradle/Map.java
@@ -6,57 +6,50 @@ import java.util.List;
 
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Map extends Application {
-    
+    //Hardcoded Scene size for testing 
+    int scene_size = 600;
+
     @Override
     public void start(Stage stage) {
         Group root  = new Group();
-        Scene scene = new Scene(root, 640, 480);
-        List<Rectangle> rectangles = new ArrayList<>();
+        Scene scene = new Scene(root, scene_size+50, scene_size+50);
+        List<List<Rectangle>> rectangles = new ArrayList<>();
 
-        createRectangles(rectangles, 5);
+        createRectangles(rectangles, scene_size/100, scene_size/100);
 
         addRectangleToRoot(root, rectangles);
-
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            
-            @Override
-            public void handle(KeyEvent event){
-                System.out.println(event.getCode());
-                if(event.getCode().toString()=="DOWN"){
-                    double rect_y = rectangles.get(0).getY();
-                    rectangles.get(0).setY(rect_y-10);;
-                }
-            }
-
-        }); 
 
         stage.setScene(scene);
         stage.show();
     }
 
     // Goes through whole list and adds rectangles to the root group
-    public void addRectangleToRoot(Group root, List<Rectangle> rectangles){
+    public void addRectangleToRoot(Group root, List<List<Rectangle>> rectangles){
         for(int i = 0;i < rectangles.size() ; i++){
-            root.getChildren().add(rectangles.get(i));
+            for(int j = 0; j < rectangles.get(i).size(); j++){
+                root.getChildren().add(rectangles.get(i).get(j));
+            }
         }
     }
 
     //Create simple boxes
-    public void createRectangles(List<Rectangle> rectangles,int rect_num){
-        for(int i = 0; i < rect_num ; i++){
-            rectangles.add(new Rectangle());    
-            rectangles.get(i).setX(50.0+i*100.0);
-            rectangles.get(i).setY(50.0);
-            rectangles.get(i).setWidth(50);
-            rectangles.get(i).setHeight(50);
+    public void createRectangles(List<List<Rectangle>> rectangles,int rect_num_x, int rect_num_y){
+            for(int i = 0; i < rect_num_y ; i++){
+                rectangles.add(new ArrayList<>());    
+
+                for(int j = 0; j < rect_num_x; j++){
+                    rectangles.get(i).add(new Rectangle());    
+                    rectangles.get(i).get(j).setX(50.0+j*100.0);
+                    rectangles.get(i).get(j).setY(50.0+i*100.0);
+                    rectangles.get(i).get(j).setWidth(50);
+                    rectangles.get(i).get(j).setHeight(50);
+                }
         }   
     }
 
