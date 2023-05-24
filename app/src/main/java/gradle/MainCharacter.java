837bea6d9155d9036071b127aa186c6f6e9b500e
Paulina Korus <paukor2606@gmail.com>
2023-05-24 11:03:16 +0200

    Adding functions

--- app/src/main/java/gradle/MainCharacter.java
+++ app/src/main/java/gradle/MainCharacter.java
@@ -2,16 +2,13 @@ package gradle;
 
 public class MainCharacter extends Character {
 
-    private static int listening_size = 10;
-    private static int scaring_level = 0;
+    private int listening_size = 10;
+    private int scaring_level = 0;
+    private int character_points = 0;
     public MainCharacter(double x, double y, String imagePath, String name) {
         super(x, y, imagePath, name);
     }
 
-    // TODO do funcions
-    public void eating() {};
-    public void getting_points() {}
-    public void moving() {};
     public boolean check_if_ghost() {
         boolean is_nerby = false;
         for(int i=0; i<Map.ghosts.size(); i++){
@@ -37,9 +34,42 @@ public class MainCharacter extends Character {
         }
         return is_nerby;
     }
-    public void escape() {};
-    public void check_if_food() {};
-    public boolean get_scared() {
+    public boolean check_if_food() {
+        boolean is_nerby = false;
+        for(int i=0; i<Map.food_list.size(); i++){
+            Food food = Map.food_list.get(i);
+            double distanceX = Math.abs(this.getMiddleX() - food.getMiddleX());
+            double distanceY = Math.abs(this.getMiddleY() - food.getMiddleY());
+
+            if (distanceX <= listening_size && this.getMiddleX() < food.getMiddleX()) {
+                setMovingDirection("RIGHT");
+                is_nerby = true;
+            } else if (distanceX <= listening_size && this.getMiddleX() > food.getMiddleX()) {
+                setMovingDirection("LEFT");
+                is_nerby = true;
+            } else if (distanceY <= listening_size && this.getMiddleY() < food.getMiddleY()) {
+                setMovingDirection("UP");
+                is_nerby = true;
+            } else if (distanceY <= listening_size && this.getMiddleY() > food.getMiddleY()) {
+                setMovingDirection("DOWN");
+                is_nerby = true;
+            }
+        }
+        return is_nerby;
+    }
+
+    public boolean eating() {
+        for(int i=0; i<Map.food_list.size(); i++){
+            Food food = Map.food_list.get(i);
+            if (this.getBoundsInParent().intersects(food.getBoundsInParent())) {
+                character_points += food.getPoints();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean getting_scared() {
         for(int i=0; i<Map.ghosts.size(); i++){
             for (int j=0; j<Map.ghosts.get(i).size(); j++) {
                 Ghost ghost = Map.ghosts.get(i).get(j);
@@ -51,5 +81,24 @@ public class MainCharacter extends Character {
         }
         return false;
     }
-    public void dying() {};
+    public boolean dying() {
+        boolean died = false;
+        if(scaring_level>=20){
+            Map.root.getChildren().remove(this);
+            died = true;
+        }
+        return died;
+    }
+
+    public int getCharacter_points(){
+        return character_points;
+    }
+
+    public int getScaring_level(){
+        return scaring_level;
+    }
+
+    public int getListening_size(){
+        return listening_size;
+    }
 }
